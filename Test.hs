7001fc28232fc49ec459e9fe8234aa80ed67ed06
Robin Green <greenrd@greenrd.org>
2009-07-20 02:27:37 +0200

    Add symlink test (POSIX only); better error messages for test failures
    Ignore-this: b6c0d831a7afb60ed533ea18113f1587
    
    darcs-hash:20090720002737-f04ea-725cc5672e0727d8f34d9ca009c0dac58adb409c

--- Test.hs
+++ Test.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE CPP #-}
 module Main where
 
 import System.IO.Cautious (writeFile)
 
 import Prelude hiding (writeFile)
-import Control.Monad (replicateM, zipWithM_)
+import Control.Monad (replicateM, zipWithM_, unless)
 import System.Directory (removeFile)
 import System.Environment (getArgs)
+#ifdef _POSIX
+import System.Posix.Files (createSymbolicLink, readSymbolicLink)
+#endif
 import System.Random (mkStdGen, randomRIO, setStdGen)
 
 -- Remember that because we are using zipWithM_ below, the lengths of testLengths and outputFilePaths need to be the same!
@@ -23,17 +27,38 @@ outputFilePaths = ["foo", "foo.txt", "./bar.txt", ".dotfile", "./.dot.dot"]
 defaultSeed :: Int
 defaultSeed = 1234
 
-testCase :: Int -> FilePath -> IO ()
-testCase dataLen outputFile = do
+stdTestCase :: Int -> FilePath -> IO ()
+stdTestCase dataLen outputFile = do
     testData <- replicateM dataLen $ randomRIO ('\0', '\255')
     writeFile outputFile testData
     testData' <- readFile outputFile
-    True <- return $ testData == testData'
+    unless (testData == testData') . fail $ "stdTestCase: failed with " ++ show dataLen ++ " " ++ show outputFile
     removeFile outputFile
+
+#ifdef _POSIX
+symlinkTestCase :: IO ()
+symlinkTestCase = do
+    let linkTarget = "link-dest"
+        linkSrc    = "link"
+        testData   = "hi"
+    writeFile linkTarget ""
+    createSymbolicLink linkTarget linkSrc
+    writeFile linkSrc testData
+    newTarget <- readSymbolicLink linkSrc
+    testData' <- readFile linkTarget
+    unless (testData == testData') $ fail "symlinkTestCase failed: data read back not the same as data written out!"
+    unless (newTarget == linkTarget) $ fail "symlinkTestCase failed: symlink clobbered"
+    mapM_ removeFile [linkSrc, linkTarget]
+#endif
 
 main :: IO ()
 main = do
     args <- getArgs
     setStdGen . mkStdGen $ if null args then defaultSeed else read (head args)
-    zipWithM_ testCase testLengths outputFilePaths
+    zipWithM_ stdTestCase testLengths outputFilePaths
+#ifdef _POSIX
+    symlinkTestCase
+#else
+    putStrLn "Warning: POSIX tests not run! If you are on a POSIX platform, then to run them type 'runhaskell -D_POSIX Test.hs'"
+#endif
     putStrLn "All cautious-file tests completed successfully!"
